6714add296f12ef9482475ed2fa9910cb9b23e46
Ryo <tzl19961216@gmail.com>
2023-02-22 23:59:09 +0800

    fix: build dist js file

--- dist/index.js
+++ dist/index.js
@@ -163,7 +163,11 @@ function run() {
             yield io.mkdirP(save_dir);
             const save_path = path.join(save_dir, file_name);
             core.info(`saving to ${save_path}`);
-            yield writeFileAsync(save_path, JSON.stringify(allReses, null, 2));
+            const save_result = {
+                archived_at: new Date().toJSON(),
+                pages: allReses
+            };
+            yield writeFileAsync(save_path, JSON.stringify(save_result, null, 2));
             core.setOutput('db_id', dbId);
             core.setOutput('save_path', save_path);
         }
